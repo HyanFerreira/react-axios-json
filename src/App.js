2c2946fa378cb2f4e716def378b63c3134db691a
HYAN <cg3024652@LAB111M03.IFSP>
2023-10-23 15:45:54 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+
+const baseURL = 'http://localhost:9000/pessoa';
+
+// let pessoas = [
+//   { COD: '01', NOME: 'Hyan Ferreira' },
+//   { COD: '02', NOME: 'Maria Eduarda' },
+//   { COD: '03', NOME: 'Helyana Ferreira' },
+// ];
+
+function MyTable() {
+  const [pessoas, setPessoas] = useState([]); // Usar um array para armazenar múltiplas pessoas
+
+  useEffect(() => {
+    axios
+      .get(baseURL)
+      .then((response) => {
+        setPessoas(response.data); // Recebe um array de pessoas
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar dados:', error);
+      });
+  }, []);
+  return (
+    <table className="table-pessoas">
+      <tr className="table-line">
+        {/* <td className="table-column">Id</td> */}
+        <td className="table-column">Nome</td>
+        <td className="table-column">Email</td>
+      </tr>
+      {pessoas.map((pessoa) => (
+        <tr className="table-line">
+          {/* <td className="table-column">{pessoa.id}</td> */}
+          <td className="table-column">{pessoa.nome}</td>
+          <td className="table-column">{pessoa.email}</td>
+        </tr>
+      ))}
+    </table>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{MyTable()}</header>
     </div>
   );
 }
